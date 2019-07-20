c432ae8f9fd69d657ac3acbc54436940d062808c
Hasan Zahid <hasanthedev@gmail.com>
2019-07-19 21:34:16 -0500

    getting the posts from database

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,12 +2,14 @@ const Post = require("../models/postModel");
 
 // controllers take care of the app logic related to a specific route
 exports.getPosts = (req, res) => {
-  res.json({
-    "posts" : [
-      {"title": "First Post"},
-      {"title": "Second Post"}
-    ]
-  })
+  // getting all posts from the databse using the find method of mongoose model
+  // we can also chain the select method to get the exact data we want, just pass the keys you want
+  const posts = Post.find().select("_id title body")
+                       .then((posts) => {
+                        // by default express send a status of 200 except when it cathces an error, so no need to explicitly specify
+                          return res.json({ posts: posts })
+                       })
+                       .catch(err => console.log(err))
 }
 
 // controller for creating a new post
@@ -18,6 +20,6 @@ exports.createPosts = (req, res) => {
   // since we are handling errors thorugh express-validator as a middleware no need to 
   // do that in the controller anymore
   post.save().then(result => {
-    res.status(200).json({ post: result })
+    res.json({ post: result })
   })
 }
